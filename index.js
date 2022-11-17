69c1e71bb5f2328e1fbc599796c4e5de67b48284
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-17 08:31:24 +0600

    make connections with mongodb.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,9 +11,25 @@ const port = process.env.PORT || 5005;
 app.use(cors());
 app.use(express.json());
 
+//===========connections
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hufticd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+//===========
+
 app.get("/", (req, res) => {
-  res.send("data is coming");
+  res.send("Welcome to AccDental server");
 });
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`AccDental server is running on port: ${port}`);
 });
